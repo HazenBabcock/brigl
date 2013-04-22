9b667e2e96fe5276c0d09e7d028af80295a6e6f1
Anon <devnull@localhost>
2013-04-22 10:08:36 +0200

    new release

--- web/js/brigl.js
+++ web/js/brigl.js
@@ -17,6 +17,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+		Revision 5:
+		- Better error handling
+		- Support for jQuery instead of prototype
+		- more options
+
 		Revision 4:
 		- Animation support
 		- Colored lines
