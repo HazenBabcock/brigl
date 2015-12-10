b21ddfd5e5b047d27a96d56fd87b180976d36303
Hazen Babcock <hbabcock@mac.com>
2015-12-09 22:39:20 -0500

    Use distutils copy_tree as it is more friendly.

--- tools/prepareParts.py
+++ tools/prepareParts.py
@@ -6,6 +6,8 @@ specified directories should not include a final forward/backward slash.
 Hazen 12/15
 """
 
+import distutils
+import distutils.dir_util
 import os
 import shutil
 import sys
@@ -13,18 +15,18 @@ import sys
 if (len(sys.argv) != 3):
     print "usage: <input directory> <output directory>"
     exit()
-    
+
 for elt in os.listdir(sys.argv[1]):
     out_dir = sys.argv[2] + os.path.sep + elt[0]
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
-    print "Processing", elt
+    print "Processing", elt, out_dir + os.path.sep + elt
     
     if os.path.isfile(sys.argv[1] + os.path.sep + elt):
         shutil.copyfile(sys.argv[1] + os.path.sep + elt,
                         out_dir + os.path.sep + elt)
     else:
-        shutil.copytree(sys.argv[1] + os.path.sep + elt,
-                        out_dir + os.path.sep + elt)
+        distutils.dir_util.copy_tree(sys.argv[1] + os.path.sep + elt,
+                                     out_dir + os.path.sep + elt)
 
