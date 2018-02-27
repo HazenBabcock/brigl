5e8967d83b81ca794d04185abb82debdf0e818d4
Hazen Babcock <hbabcock@mac.com>
2018-02-26 23:34:38 -0500

    Capture log from the browser and parse for 'SEVERE' errors. Switch to a different test for each standard example instead of trying to test them all in a single test.

--- test/get_driver.py
+++ test/get_driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
-This should returns an automated web browser to use for automated testing.
+This returns an automated web browser to use for automated testing. It also 
+includes some utility functions.
 
 https://www.seleniumhq.org/
 http://selenium-python.readthedocs.io/
@@ -8,17 +9,41 @@ http://selenium-python.readthedocs.io/
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities  
 
-
+# Web browser interface.
 def getDriver():
     """
     This is configured to use Chrome, change as desired.
     """
+    desired = DesiredCapabilities.CHROME
+    desired['loggingPrefs'] = {'browser' : 'ALL'}
+    
     options = webdriver.ChromeOptions()
     options.add_argument("--window-size=1000,1000")
 
     driver = webdriver.Chrome(executable_path="./chromedriver",
+                              desired_capabilities = desired,
                               chrome_options = options)
 
     return driver
+
+
+# Utility functions.
+def parseLog(log_data, level = 'SEVERE'):
+    """
+    Return only those messages with the specified level.
+    """
+    temp = []
+    for elt in log_data:
+        if (elt['level'] == level):
+            temp.append(elt)
+    return temp
+
+def pprintLog(log_data):
+    """
+    Pretty print log messages.
+    """
+    for elt in log_data:
+        print(elt)
+        
--- test/test_standard_examples.py
+++ test/test_standard_examples.py
@@ -8,28 +8,59 @@ import sys
 import configure as configure
 import get_driver as getDriver
 
-examples = ["simple.html",
-            "multiple.html",
-            "copypaste.html"]
-
-def test_examples():
+def test_simple():
     driver = getDriver.getDriver()
-    for example in examples:
-        driver.get(configure.www_root + example)
-        log = driver.find_element_by_id("logarea")
+    driver.get(configure.www_root + "simple.html")
+    log = driver.find_element_by_id("logarea")
 
-        worked = False
-        for i in range(20):
-            if ("Model loaded successfully" in log.text):
-                worked = True
-                break
-            time.sleep(0.5)
+    worked = False
+    for i in range(20):
+        if ("Model loaded successfully" in log.text):
+            worked = True
+            break
+        time.sleep(0.5)
 
-        print(log.text)
-        assert(worked)
+    assert(worked)
+    log_data = driver.get_log('browser')
+    print(getDriver.pprintLog(getDriver.parseLog(log_data)))
     driver.close()
 
+def test_multiple():
+    driver = getDriver.getDriver()
+    driver.get(configure.www_root + "multiple.html")
+    log = driver.find_element_by_id("logarea")
+
+    worked = False
+    for i in range(20):
+        if ("Model loaded successfully" in log.text):
+            worked = True
+            break
+        time.sleep(0.5)
+
+    assert(worked)
+    log_data = driver.get_log('browser')
+    print(getDriver.pprintLog(getDriver.parseLog(log_data)))
+    driver.close()
+
+def test_copypaste():
+    driver = getDriver.getDriver()
+    driver.get(configure.www_root + "copypaste.html")
+    log = driver.find_element_by_id("log")
+    
+    worked = False
+    for i in range(20):
+        if ("Model loaded successfully" in log.get_attribute("value")):
+            worked = True
+            break
+        time.sleep(0.5)
+
+    assert(worked)
+    log_data = driver.get_log('browser')
+    print(getDriver.pprintLog(getDriver.parseLog(log_data)))
+    driver.close()    
 
 if (__name__ == "__main__"):
-    test_examples()
+#    test_simple()
+#    test_multiple()
+    test_copypaste()
     
