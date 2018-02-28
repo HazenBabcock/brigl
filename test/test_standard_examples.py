505eaf632015bc4e334440d4ff944b0a094a2ab5
Hazen Babcock <hbabcock@mac.com>
2018-02-27 22:56:52 -0500

    Add test of anim.html.

--- test/test_standard_examples.py
+++ test/test_standard_examples.py
@@ -5,9 +5,15 @@ Load all the standard examples to check that they are not broken.
 import time
 import sys
 
+import selenium
+from selenium.webdriver.support.ui import Select
+
 import configure as configure
 import driver as driver
 
+
+# Tests are in the order of the examples on the index.html page.
+#
 def test_simple():
     www_driver = driver.getDriver()
     www_driver.get(configure.www_root + "simple.html")
@@ -72,8 +78,49 @@ def test_steps():
     driver.noSevereErrors(www_driver)
     www_driver.close()
 
+def test_lines():
+    www_driver = driver.getDriver()
+    www_driver.get(configure.www_root + "lines.html")
+    log = www_driver.find_element_by_id("logarea")
+
+    worked = False
+    for i in range(20):
+        if ("Model loaded successfully" in log.text):
+            worked = True
+            break
+        time.sleep(0.5)
+
+    assert(worked)
+    driver.noSevereErrors(www_driver)
+    www_driver.close()
+
+def test_animation():
+    www_driver = driver.getDriver()
+    www_driver.get(configure.www_root + "anim.html")
+    log = www_driver.find_element_by_id("logarea")
+    select = Select(www_driver.find_element_by_xpath("/html/body/select[1]"))
+
+    for option in select.options:
+        option.click()
+
+        worked = False
+        for i in range(20):
+            if ("Model loaded successfully" in log.text):
+                worked = True
+                break
+            time.sleep(0.5)
+        
+        assert(worked)
+        driver.noSevereErrors(www_driver)
+        
+    www_driver.close()       
+
 if (__name__ == "__main__"):
     test_simple()
     test_multiple()
     test_copypaste()
     test_steps()
+    test_lines()
+    test_animation()
+    
+    
